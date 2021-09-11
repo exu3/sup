17ef2e0fc90dcd95ba99994777ae0213f7122b4a
eilla1 <git@ella.cx>
2021-09-11 00:38:13 -0700

    Bot spam for deer

---
+++ pages/api/internals.js
@@ -0,0 +1,20 @@
+import { WebClient } from "@slack/web-api";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  const client = new WebClient();
+
+  const d = new Date();
+  const minute = d.getMinutes();
+
+  const slackRequest = await client.chat.postMessage({
+    token: process.env.SLACK_TOKEN,
+    channel: process.env.INTERNALS_CHANNEL_ID,
+    text: `
+	yeah (${minute} minutes)
+	
+_Sent via <#C02EA7XCGKW>_
+	`,
+  });
+  res.send("thump thump");
+};
